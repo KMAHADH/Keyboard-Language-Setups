b5b1a55add8656cdcdf589aeb11dadc0d4e862e0
Khwaja Mahad Haq <55731504+KMAHADH@users.noreply.github.com>
2021-02-10 00:11:35 -0500

    Add files via upload

---
+++ German Keyboard.sh
@@ -0,0 +1,8 @@
+echo keysym Alt_L = Mode_switch >> ~/.Xmodmap
+echo keysym A = a A adiaeresis Adiaeresis >> ~/.Xmodmap
+echo keysym E = e E ediaeresis Ediaeresis >> ~/.Xmodmap
+echo keysym I = i I idiaeresis Idiaeresis >> ~/.Xmodmap
+echo keysym O = o O odiaeresis Odiaeresis >> ~/.Xmodmap
+echo keysym U = u U udiaeresis Udiaeresis >> ~/.Xmodmap
+echo keysym Y = y Y udiaeresis Ydiaeresis >> ~/.Xmodmap
+echo keysym S = s S ssharp ssharp >> ~/.Xmodmap
